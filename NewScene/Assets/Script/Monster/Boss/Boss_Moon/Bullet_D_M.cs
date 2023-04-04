fa2ba91eecb8483e8ce69d94d0e40a4a67e2c173
kitsunera1138 <kitsunera1138@gmail.com>
2023-04-04 22:47:09 +0900

    Boss_Moon add
    Boss_Moon add_

---
+++ NewScene/Assets/Script/Monster/Boss/Boss_Moon/Bullet_D_M.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_D : MonoBehaviour
+{
+    public GameObject Rock;
+    [SerializeField]
+    float spawntime;
+    float time;
+
+    [SerializeField]
+    bool Spawner;
+    [SerializeField]
+    bool Attacker;
+    private void Start()
+    {
+        time = 0;
+    }
+    private void Update()
+    {
+        if (Spawner)
+        {
+            time += Time.deltaTime;
+
+            if (time > spawntime)
+            {
+                Debug.Log("sssssssss");
+                //GameObject Skill1 = Instantiate(Rock, transform.position, transform.rotation);
+                Rock.SetActive(true);
+                time = 0;
+            }
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Main_gangrim")
+        {
+
+            if (Attacker)
+            {
+
+                Debug.Log("Damage for ballet");
+                Destroy(gameObject);
+                //대충 플레이어가 대미지 입었다는 코드 sendMessage등 으로 Player 스크립트에 접근
+                //나중에 플레이어 체력 정해지면 추가
+            }
+
+
+        }
+
+    }
+}
---
+++ NewScene/Assets/Script/Monster/Boss/Boss_Moon/SpawnDoonMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnDoonMove : MonoBehaviour
+{
+    public float speed; //임의 20
+    public Rigidbody PatternBullet;
+
+    bool bullettime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z <= 20)
+        {
+
+            if (!bullettime)
+            {
+                bullettime = true;
+                StartCoroutine(Pattern3BulletTime_()); //총알 위 아래 발사 코루틴
+            }
+
+            transform.Translate(-1 * speed * Time.deltaTime, 0, 0); //돌아가 있는데 축 때문에 -1이동
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    IEnumerator Pattern3BulletTime_()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        Rigidbody bullet = Instantiate(PatternBullet, transform.position, transform.rotation);
+        bullet.velocity = transform.forward * 10;
+        //bullet.transform.Translate(Vector3.forward * Time.deltaTime * 10);
+
+        Rigidbody bulle2t = Instantiate(PatternBullet, transform.position, transform.rotation);
+        bulle2t.velocity = transform.forward * -10;
+        //bulle2t.transform.Translate(Vector3.forward * Time.deltaTime * 10);
+        bullettime = false;
+    }
+}
