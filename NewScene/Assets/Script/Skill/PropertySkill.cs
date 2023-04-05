17e05d985903d6ea1231ca284645e9acb4250436
woojun0037 <woojun0037@naver.com>
2023-04-06 04:32:41 +0900

    04.06_1
    Skill mixing buff

--- NewScene/Assets/Script/Skill/PropertySkill.cs
+++ NewScene/Assets/Script/Skill/PropertySkill.cs
@@ -2,39 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class  PropertySkill : MonoBehaviour
+public class PropertySkill : MonoBehaviour
 {
+    [SerializeField] private float BuffTime = 6f;
     Main_Player SkillUse;
-    public float SpeedUp;
-
-    private bool TafoonSkillOn;
-    private bool TafoonSkillOff;
+    public float SpeedUp = 1.2f;
 
     private void Start()
     {
         SkillUse = GetComponent<Main_Player>();
     }
 
+    private void Update()
+    {
+        TafoonSkill();
+    }
+
     public void TafoonSkill()
     {
-        if(SkillUse.E_skillCheck == true && SkillUse.R_skillCheck == true)
+        if (SkillUse.E_skillCheck == true && SkillUse.R_skillCheck == true)
         {
-            StartCoroutine(TafoonSpecial());
+            SkillUse.E_skillCheck = false;
+            SkillUse.R_skillCheck = false;
+
+            SkillUse.MoveSpeed +=5f;
+            SpeedUp += 1f;
+
+            SkillUse.Anim.SetFloat("AttackSpeed_1", SpeedUp);
+            SkillUse.Anim.SetFloat("AttackSpeed_2", SpeedUp);
+            SkillUse.Anim.SetFloat("AttackSpeed_3", SpeedUp);
+
+            StartCoroutine(TafoonSkillUse());
         }
     }
 
-    IEnumerator TafoonSpecial()
+    IEnumerator TafoonSkillUse()
     {
-        SkillUse.MoveSpeed += 5;
-        SkillUse.Anim.SetFloat("AttackSpeed_1", SpeedUp + 2f);
-        SkillUse.Anim.SetFloat("AttackSpeed_2", SpeedUp + 2f);
-        SkillUse.Anim.SetFloat("AttackSpeed_3", SpeedUp + 2f);
-        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(BuffTime);
+        SkillUse.MoveSpeed = 6f;
+        SpeedUp = 1.2f;
+
+        SkillUse.Anim.SetFloat("AttackSpeed_1", SpeedUp);
+        SkillUse.Anim.SetFloat("AttackSpeed_2", SpeedUp);
+        SkillUse.Anim.SetFloat("AttackSpeed_3", SpeedUp);
     }
 
     public void IceSkill()
     {
-        
+
     }
 
     public void Thunder()
