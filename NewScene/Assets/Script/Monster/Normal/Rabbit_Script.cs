3885ee3e9f9ab03fa2f76819e2c70cfb2afd2961
kitsunera1138 <kitsunera1138@gmail.com>
2023-04-11 23:48:13 +0900

    enemy
    enemy

--- NewScene/Assets/Script/Monster/Normal/Rabbit_Script.cs
+++ NewScene/Assets/Script/Monster/Normal/Rabbit_Script.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rabbit_Script : MonoBehaviour
+public class Rabbit_Script : Enemy
 {
     [SerializeField]
     GameObject attacker_Col;
@@ -13,10 +13,6 @@ public class Rabbit_Script : MonoBehaviour
     bool DontMove;
     Transform targettransform;
 
-    [SerializeField]
-    float MaxDistance;
-    [SerializeField]
-    float chasespeed;
     RaycastHit hit;
     [SerializeField]
     private float Distance;
@@ -24,10 +20,15 @@ public class Rabbit_Script : MonoBehaviour
     float time;
     
     public float monsterhp;
-
-    // Start is called before the first frame update
-    void Start()
+    protected override void Awake()
     {
+        base.Awake();
+        isattack = false;
+    }
+
+    protected override void Start()
+    {
+        base.Start();
         time = 0;
         animator = GetComponent<Animator>();
         animator.SetBool("Idle", true);
@@ -37,102 +38,105 @@ public class Rabbit_Script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Distance = Vector3.Distance(targettransform.transform.position, transform.position);
+        DieMonster();
+        monsterMove();
+        NotDamaged();
+    }
 
-        time += Time.deltaTime;
-        Vector3 rayspawn = transform.position;
-        rayspawn.y = SetY; //레이 바닥에서 부터 쏘게만듬
 
-        Vector3 targety = targettransform.position;
+    protected override void monsterMove()
+    {
+        animator.SetBool("Move", true);
+        base.monsterMove();
+
+        Vector3 targety = targetTransform.position;
         targety.y = SetY;
 
-
-        Debug.DrawRay(rayspawn, transform.forward * MaxDistance, Color.blue, 0.05f);
-
-        if (Physics.Raycast(rayspawn, transform.forward, out hit, MaxDistance))
+        //현재 경로에서 목표 지점까지 남아있는 거리
+        if (agent.remainingDistance <= 2)
         {
-            if (hit.collider.tag == "Main_gangrim") //플레이어에게 레이 닿으면 정지하고 총알 공격
+            if (!isattack)
             {
-                if(Distance > 1f)
-                {
-                    time = 0;
-                    //Debug.Log("Main tag");
-                    DontMove = true;
-                    if (!isattack)
-                    {
-                        isattack = true;
-                        transform.LookAt(targety);
-                        animator.SetBool("Attack", true);
-                        StartCoroutine("attacker");
-
-                    }
-                }
-                else  //거리가 일정 이상 가까우면 정지
-                {
-
-                    animator.SetBool("Idle", true);
-                    animator.SetBool("Move", false);
-
-                    transform.LookAt(targety);
-
-                    if(time > 3f)//공격안하는 버그 방지
-                    {
-                        //StartCoroutine("attacker");
-                        time = 0;
-                    }
-                }
-
-            }
-
-        }
-        else
-        {
-            DontMove = false;
-        }
-
-        if (!DontMove)
-        {
-            if (Distance > 1f && !meetplayer)
-            {
-                animator.SetBool("Idle", true);
-                animator.SetBool("Move", true);
-                //agent.destination = targety;
-
-                transform.position = Vector3.MoveTowards(gameObject.transform.position, targety, chasespeed * Time.deltaTime);//3번째 값 돌진 속도
                 transform.LookAt(targety);
+                isattack = true;
+                StartCoroutine("attacker");
             }
+        }
 
-        }
-        else
-        {
-            animator.SetBool("Move", false);
-        }
+
+        //Distance = Vector3.Distance(targettransform.transform.position, transform.position);
+
+        //time += Time.deltaTime;
+        //Vector3 rayspawn = transform.position;
+        //rayspawn.y = SetY; //레이 바닥에서 부터 쏘게만듬
+
+        //Vector3 targety = targettransform.position;
+        //targety.y = SetY;
+
+
+        //Debug.DrawRay(rayspawn, transform.forward * MaxDistance, Color.blue, 0.05f);
+
+        //if (Physics.Raycast(rayspawn, transform.forward, out hit, MaxDistance))
+        //{
+        //    if (hit.collider.tag == "Main_gangrim") //플레이어에게 레이 닿으면 정지하고 총알 공격
+        //    {
+        //        if (Distance > 1f)
+        //        {
+        //            time = 0;
+        //            //Debug.Log("Main tag");
+        //            DontMove = true;
+        //            if (!isattack)
+        //            {
+        //                isattack = true;
+        //                transform.LookAt(targety);
+        //                animator.SetBool("Attack", true);
+        //                StartCoroutine("attacker");
+
+        //            }
+        //        }
+        //        else  //거리가 일정 이상 가까우면 정지
+        //        {
+
+        //            animator.SetBool("Idle", true);
+        //            animator.SetBool("Move", false);
+
+        //            transform.LookAt(targety);
+
+
+        //        }
+
+        //    }
+
+        //}
+        //else
+        //{
+        //    DontMove = false;
+        //}
+
+        //if (!DontMove)
+        //{
+        //    if (Distance > 1f && !meetplayer)
+        //    {
+        //        animator.SetBool("Idle", true);
+        //        animator.SetBool("Move", true);
+        //        //agent.destination = targety;
+
+        //        transform.position = Vector3.MoveTowards(gameObject.transform.position, targety, chasespeed * Time.deltaTime);//3번째 값 돌진 속도
+        //        transform.LookAt(targety);
+        //    }
+
+        //}
+        //else
+        //{
+        //    animator.SetBool("Move", false);
+        //}
 
     }
 
-    void MonsterDamage() //플레이어 스크립트에서 불러옴
-    {
-        Debug.Log("MonsterDamage()");
 
-        if (monsterhp > 1)
-        {
-            monsterhp--;
-        }
-        else
-        {
-            MonsterDie();
-        }
-        Debug.Log(monsterhp);
-    }
-
-    void MonsterDie()
-    {
-        Destroy(gameObject);
-    }
 
     IEnumerator attacker()
     {
-        meetplayer = true;
         animator.SetBool("Attack", true);
         attacker_Col.SetActive(true);
         yield return new WaitForSeconds(1.3f);
@@ -140,17 +144,7 @@ public class Rabbit_Script : MonoBehaviour
         animator.SetBool("Attack", false);
         yield return new WaitForSeconds(0.7f);
         isattack = false;
-        meetplayer = false;
+
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Main_gangrim")
-        {
-            //플레이어에 피격 매서드 불러옴
-            //playercollider = true;
-
-            Debug.Log("player collision");
-        }
-    }
 }
