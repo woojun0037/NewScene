9d626c0123dd71d2e5f27e143b96cb2f311d2529
kitsunera1138 <kitsunera1138@gmail.com>
2023-04-19 00:37:06 +0900

    map_add
    map_add

--- NewScene/Assets/Scenes/MainCamera.cs
+++ NewScene/Assets/Scenes/MainCamera.cs
@@ -5,40 +5,16 @@ using UnityEngine;
 public class MainCamera : MonoBehaviour
 {
     [SerializeField] Transform FollowTarget;
+
     public Vector3 FollowPlayer;
-    private bool isShake;
+
     void Start()
     {
-        isShake = false;
         FollowPlayer = this.transform.position - FollowTarget.position;
     }
 
     void LateUpdate()
     {
-        if(isShake == false)
-            this.transform.position = FollowTarget.transform.position + FollowPlayer;
-    }
-
-    public IEnumerator Shake(float Duration, float magnitude)
-    {
-        isShake=true;
-        Vector3 originalPos = transform.localPosition;
-
-        float elapsed = 0.0f;
-
-        while (elapsed < Duration)
-        {
-            
-            float x = Random.Range(-1f, 1f) * magnitude;
-            float y = Random.Range(-1f, 1f) * magnitude;
-
-            transform.localPosition = new Vector3(originalPos.x - x, originalPos.y - y, originalPos.z);
-
-            elapsed += Time.deltaTime;
-
-            yield return null; 
-        }
-        transform.localPosition = originalPos;
-        isShake = false;
+        this.transform.position = FollowTarget.transform.position + FollowPlayer;
     }
 }
