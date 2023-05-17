ac67f25173997a4132843fa5ac0726007736ed82
kitsunera1138 <kitsunera1138@gmail.com>
2023-05-18 04:00:50 +0900

    2
    2

--- NewScene/Assets/Script/Monster/Normal/Golem_Script.cs
+++ NewScene/Assets/Script/Monster/Normal/Golem_Script.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Golem_Script : MonoBehaviour
+public class Golem_Script : Enemy
 {
     // Start is called before the first frame update
     void Start()
