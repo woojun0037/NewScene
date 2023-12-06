d4c60556a4f8782f0603976e66220a88ee83a23a
kitsunera1138 <kitsunera1138@gmail.com>
2023-12-06 22:11:57 +0900

    DDD
    DDD

--- NewScene/Assets/JIHO/Scripts/TitleManager.cs
+++ NewScene/Assets/JIHO/Scripts/TitleManager.cs
@@ -5,8 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour
 {
+    public GameObject CreaditUI;
+    bool CreaditOn;
+    public GameObject[] ClickText;
+
     public void GameStart()
     {
+        if(!CreaditOn)
         SceneManager.LoadScene("ChatScene");
+    }
+
+
+    public void ToggleUIPanel()
+    {
+        CreaditUI.SetActive(!CreaditUI.activeSelf);
+        foreach (GameObject obj in ClickText)
+        {
+            obj.SetActive(!obj.activeSelf);
+        }
     }
 }
---
+++ NewScene/Assets/Script/UI/CreaditSceneMove.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreaditSceneMove : MonoBehaviour
+{
+    public void CreditMove()
+    {
+        //SceneManager.LoadScene("EndingCreadit");
+    }
+}
